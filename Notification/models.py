afae9f85fe55666a7b695e73409e79a1bc40e954
lubna219 <22201219@uap-bd.edu>
2025-04-25 23:29:52 +0600

    notification model created

--- Notification/models.py
+++ Notification/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import user
 
 # Create your models here.
+class Notification(models.model):
+    name = models.Foreignkey(Course,on_delete = models.CASCADE)
+    type = models.Charfield(max_length = 100,null = True,blank = True)
+    time = models.TimeField(null = True, blank = True)
+    message = models.Textfield(max_length = 500,null = True,blank = True)
+
+
+    def __str__(self):
+        return self.name
