d3da25a5586548bbe55026b4b39aa2f70212c984
lubna219 <22201219@uap-bd.edu>
2025-04-26 18:43:36 +0600

    notification fully done

--- Notification/models.py
+++ Notification/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 class Notification(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=255, default='No Title')
+    message = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)  
+
+    def _str_(self):
+        return self.title
--- Notification/views.py
+++ Notification/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
+from .models import Notification
 
-def notification(request):
-    return render(request, 'notification.html') 
-
-
+def notification_list(request):
+    notifications = Notification.objects.all().order_by('-created_at')
+    return render(request, 'notification.html', {'notifications': notifications})
