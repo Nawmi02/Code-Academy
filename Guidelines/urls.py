23593d3468c5e6ef9e79283bf6261605c7d13ded
Nawmi02 <nawmizakir@gmail.com>
2025-04-26 15:55:39 +0600

    error fixed

--- Guidelines/urls.py
+++ Guidelines/urls.py
@@ -2,10 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< Updated upstream
     path('', views.show_guidelines, name='guidelines'),
 ]
-=======
-    path('', views.guideline, name='guidelines'),
-]
->>>>>>> Stashed changes
+
+
--- Guidelines/views.py
+++ Guidelines/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render
 from .models import Guideline
 
-<<<<<<< Updated upstream
 def show_guidelines(request):
     all_guidelines = Guideline.objects.all()
     return render(request, 'guidelines.html', {'guidelines': all_guidelines})
-=======
-def guideline(request):
-    return render(request, 'guidelines.html') 
-
-
->>>>>>> Stashed changes
