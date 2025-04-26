faf7a4f73ade0c15aa9d71194262f0d7d12fd247
unknown <njahanurmi3@gmail.com>
2025-04-26 14:27:45 +0600

    some changes idk

--- Guidelines/models.py
+++ Guidelines/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Guigiline(models.Model):
+class Guideline(models.Model):
     name = models.Foreignkey(Course,on_delete = models.CASCADE)
     topic = models.Charfield(max_length = 100,null = True, blank = True)
     text = models.Textfield(max_length = 100,null = True, blank = True)
