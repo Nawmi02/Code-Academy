fa8848d93777aeb34f90a932d2ff724400f4e8eb
Nawmi02 <nawmizakir@gmail.com>
2025-05-01 22:56:53 +0600

    Read all done

--- Notification/models.py
+++ Notification/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Notification(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, null=True, blank=True)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
