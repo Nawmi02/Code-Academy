d2b660b5b5bb19b6b5b46e9437b0e249e5d76f00
Nawmi02 <nawmizakir@gmail.com>
2025-05-30 18:40:13 +0600

    Procfile

--- CodeAcademy/settings.py
+++ CodeAcademy/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR/'static',]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
